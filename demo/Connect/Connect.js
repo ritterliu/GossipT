1c4d347feaf99692d1a6ca3b8665b361b7f0b6fc
Ritter Liu <ritterliu@gmail.com>
2015-02-08 23:52:40 +0800

    Update demo.
    Signed-off-by: Ritter Liu <ritterliu@gmail.com>

--- demo/Connect/Connect.js
+++ demo/Connect/Connect.js
@@ -1,4 +1,31 @@
 var connect = require('connect');
+var timer = require('./request_time');
 var server = connect.createServer();
-server.use(connect.static(__dirname + '/websites'));
+server.use(connect.logger('dev'));
+server.use(connect.logger(' :remote-addr'));
+server.use(timer({time:500}));
+
+server.use(function (req, res, next){
+    console.log("AAAAA");
+    if('/a' == req.url) {
+        res.writeHead(200);
+        res.end('Fast!');
+    } else {
+        next();
+    }
+});
+
+server.use(function (req, res, next){
+    console.log("BBBBB");
+    if('/b' == req.url) {
+        setTimeout(function(){
+            res.writeHead(200);
+            res.end('Slow!');
+        }, 1000);
+    } else {
+        next();
+    }
+});
+
+
 server.listen(3000);
--- demo/Connect/request_time.js
+++ demo/Connect/request_time.js
@@ -1,4 +1,18 @@
 module.exports = function (opts) {
-	var time = opts.time || 100
-	
-}
+        var time = opts.time || 100
+        console.log("request_time:" + time);
+        return function (req, res, next) {
+                var timer = setTimeout(function() {
+                        console.log('Too long', req.method, req.url);
+                }, time);
+
+                var end = res.end;
+                res.end = function(chunk, encoding){
+                        res.end = end;
+                        res.end(chunk, encoding);
+                        clearTimeout(timer);
+                };
+                next();
+                console.log("request_time: End" );
+        };
+};
